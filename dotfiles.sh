9dbd3146281503ae6aedf35e5cfc5b185a24c3b6
Alejandro Druetta <aledruetta@gmail.com>
2020-04-11 20:52:52 -0300

    dotfiles per distro

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,44 +4,44 @@
 #   - Uncomment desired call funtions into main
 #   - ./dotfiles.sh
 
-dotfilesDir=$(pwd)
-i=0
+distro="$1"
 
 function linkDotfile {
-  dest="${1}/.${2}"
+  dest="${1}/.${3}"
   dateStr="$(date +%Y-%m-%d-%H%M)"
 
   if [ -L "${dest}" ]; then
-    # Existing symlink
-    echo "[$((++i))] Removing existing symlink: ${dest}"
+    # Removing existing symlink
+    echo "Symlink: removing from ${dest}"
     rm "${dest}"
 
   elif [ -f "${dest}" ]; then
-    # Existing file
-    echo "[$((++i))] Backing up existing file: ${dest}"
+    # Backing up existing file
+    echo "File: backing up from ${dest}"
     mv "${dest}"{,."${dateStr}"}
 
   elif [ -d "${dest}" ]; then
-    # Existing dir
-    echo "[$((++i))] Backing up existing dir: ${dest}"
+    # Backing up existing dir
+    echo "Directory: backing up from ${dest} as ${dest}.${dateStr}"
     mv "${dest}"{,."${dateStr}"}
   fi
 
-  echo "[$((++i))] Creating new symlink: ${dest}"
-  ln -s "${dotfilesDir}/${2}" "${dest}"
+  # Creating new symlink
+  echo "Symlink: creating new at ${dest}"
+  ln -s "$(pwd)/${2}/${3}" "${dest}"
 }
 
 function main {
   echo "Start"
-  # linkDotfile "$HOME" vimrc
-  # linkDotfile "$HOME" tmux.conf
-  # linkDotfile "$HOME" bashrc
-  # linkDotfile "$HOME" bash_alias
-  # linkDotfile "$HOME" profile
-  # linkDotfile "$HOME" gitconfig
-  # linkDotfile "$HOME" gitignore_global
-  # linkDotfile "$HOME" zshrc
-  # linkDotfile "$HOME"/.vim ycm_extra_conf.py
+  linkDotfile "$HOME" common vimrc
+  linkDotfile "$HOME" common tmux.conf
+  linkDotfile "$HOME" common bash_alias
+  linkDotfile "$HOME" common gitconfig
+  linkDotfile "$HOME" common gitignore_global
+  linkDotfile "$HOME" common zshrc
+  linkDotfile "${HOME}/.vim" common ycm_extra_conf.py
+  linkDotfile "$HOME" "${distro}" bashrc
+  linkDotfile "$HOME" "${distro}" profile
   echo "End"
 }
 
